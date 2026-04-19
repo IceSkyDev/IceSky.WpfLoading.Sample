ebabbb0689bd58c3583364280318aed0ca193564
IceSky <361507120@qq.com>
2026-04-19 22:51:23 +0800

    add help

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Diagnostics;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -19,6 +20,11 @@ namespace IceSky.WpfLoading.Sample
         public MainWindow()
         {
             InitializeComponent();
+            btnHelp.Click += (o, e) =>
+            {
+                var url = "https://iceskydev.github.io/AppDoc/dev/en/WpfLoading.html";
+                Process.Start(new ProcessStartInfo("cmd", $"/c start {url}") { CreateNoWindow = true });
+            };
         }
     }
 }
